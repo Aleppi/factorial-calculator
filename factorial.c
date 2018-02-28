4c54965036f57ae46d7fbde8ba7004d062fd710c
Hector Tiblom <hectortib@gmail.com>
2018-02-28 23:12:47 +0100

    Began sanitizing input

--- factorial.c
+++ factorial.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 
-unsigned long long int calculateFactorial(long long int x)
+enum _bool
 {
-    unsigned long long int result = 1;
-    for (unsigned long long int i = x; i > 1; --i)
+	false = 0,
+	true = 1
+};
+
+typedef enum _bool Bool;
+
+unsigned long long readUserInput(void)
+{
+	unsigned long long num;
+	scanf("%llu", &num);
+	char s[MAX_LINE];
+	Bool valid = false;
+	fgets(s, sizeof(s), stdin);
+	int len = strlen(s);
+	while (len > 0 && isspace(s[len - 1]))
+	{
+		if (len > 0)
+		{
+			valid = true;
+			for (i = 0; i < len; ++i)
+			{
+				if (!isdigit([i]))
+				{
+					valid = false;
+					break;
+				}
+			}
+		}
+	}
+	if (valid == false)
+		printf("Please enter an integer.\n");
+	sscanf(s, "%d", num);
+	return num;
+}
+unsigned long long calculateFactorial(unsigned long long x)
+{
+    unsigned long long result = 1;
+    for (unsigned long long i = x; i > 1; --i)
     {
         result *= i;
     }
@@ -13,9 +50,8 @@ unsigned long long int calculateFactorial(long long int x)
 int main(void)
 {
     printf("Enter the number to calculate the factorial of: ");
-    unsigned long long int x;
-    scanf("%llu", &x);
-    unsigned long long int xFactorial = calculateFactorial(x);
+    unsigned long long x = readUserInput();
+    unsigned long long xFactorial = calculateFactorial(x);
     printf("%d! = %llu\n", x, xFactorial);
     return 0;
 }
